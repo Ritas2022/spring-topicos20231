11532ff1c9b7ec8dfae18fcdd955374953d5f7d1
Rita de Cássia Ferreira <rita.ferreira2529@gmail.com>
2023-04-10 11:43:26 +0000

    Consultas

--- src/main/java/br/gov/sp/fatec/springtopicos20231/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20231/repository/UsuarioRepository.java
@@ -6,4 +6,6 @@ import br.gov.sp.fatec.springtopicos20231.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+    public Usuario findByNome(String nome);
+
 }
